3830b3611ccdd38693a46e30734fab31bc7f4faf
Alicia <aliciadouglas26@gmail.com>
2018-09-24 14:28:15 -0400

    added documentation to all methods in CalendarService.java

--- src/main/java/com/revature/services/CalendarService.java
+++ src/main/java/com/revature/services/CalendarService.java
@@ -13,90 +13,157 @@ import com.revature.repositories.CalendarEventRepository;
 import com.revature.repositories.CalendarSubtopicRepository;
 
 /**
- * This is the service that calls repository methods from CalendarCurriculumRepository, CalendarEventRepository, and CalendarSubtopicRepository to implement calendar CRUD functionality.  
- * It implements CRUD functionality for calendar curriculums, calendar events, and calendar subtopics.
- * There is no functionality to delete records from the database.
- * The methods in this class are called from the CalendarController class methods. They call repository methods to carry out CRUD functionality.
- * @author author Derek Loisel batch 1806-jun18-usf-java trainer Wezley Singleton
+ * This is the service that calls repository methods from
+ * CalendarCurriculumRepository, CalendarEventRepository, and
+ * CalendarSubtopicRepository to implement calendar CRUD functionality. It
+ * implements CRUD functionality for calendar curriculums, calendar events, and
+ * calendar subtopics. There is no functionality to delete records from the
+ * database. The methods in this class are called from the CalendarController
+ * class methods. They call repository methods to carry out CRUD functionality.
+ * 
+ * @author author Derek Loisel batch 1806-jun18-usf-java trainer Wezley
+ *         Singleton
  */
 
 @Service
 @Transactional
 public class CalendarService {
 
-	//inject the repositories used by this service
 	@Autowired
 	CalendarCurriculumRepository calendarCurriculumRepo;
 	@Autowired
 	CalendarEventRepository calendarEventRepo;
 	@Autowired
 	CalendarSubtopicRepository calendarSubtopicRepo;
-	
-	//find all calendar curriculums, events, or subtopics
-	//curriculums
+
+	/**
+	 * This method returns all Calendar curriculums in the database
+	 * 
+	 * @return List<CalendarCurriculum> a list of all curriculum
+	 */
 	public List<CalendarCurriculum> findAllCalendarCurriculums() {
 		return calendarCurriculumRepo.findAll();
 	}
-	//events
+
+	/**
+	 * This method Returns all Calendar events in the database
+	 * 
+	 * @return List<CalendarEvent> a list of all calendar events
+	 */
 	public List<CalendarEvent> findAllCalendarEvents() {
 		return calendarEventRepo.findAll();
 	}
-	//subtopics
+
+	/**
+	 * This method Returns all Calendar subtopics in the database
+	 * 
+	 * @return List<CalendarSubtopic> a list of all calendar subtopics
+	 */
 	public List<CalendarSubtopic> findAllCalendarSubtopics() {
 		return calendarSubtopicRepo.findAll();
 	}
-	
-	//find a calendar curriculum, event, or subtopic by id
-	//curriculum
+
+	/**
+	 * This method Returns the calendar curriculum that corresponds with the id parameter
+	 * 
+	 * @param int id
+	 * @return CalendarCurriculum
+	 */
 	public CalendarCurriculum findCalendarCurriculumById(Integer id) {
 		return calendarCurriculumRepo.findCalendarCurriculumById(id);
 	}
-	//event
+
+	/**
+	 * This method Returns the calendar event that corresponds with the id parameter
+	 * 
+	 * @param int id
+	 * @return CalendarEvent
+	 */
 	public CalendarEvent findCalendarEventById(Integer id) {
 		return calendarEventRepo.findCalendarEventById(id);
 	}
-	//subtopic
+
+	/**
+	 * This method Returns the calendar subtopic that corresponds with the id parameter
+	 * 
+	 * @param int id
+	 * @return CalendarSubtopic
+	 */
 	public CalendarSubtopic findCalendarSubtopicById(Integer id) {
 		return calendarSubtopicRepo.findCalendarSubtopicById(id);
 	}
-	
-	//add a calendar curriculum, event, or subtopic
-	//curriculum
+
+	/**
+	 * This method takes a CalendarCurriculum object and persists it to a database.
+	 * 
+	 * @param CalendarCurriculum newCalendarCurriculum
+	 * @return Calendar
+	 */
 	public CalendarCurriculum addCalendarCurriculum(CalendarCurriculum newCalendarCurriculum) {
 		return calendarCurriculumRepo.save(newCalendarCurriculum);
 	}
-	//event
+
+	/**
+	 * This method takes a CalendarEvent object and persists it to a database.
+	 * 
+	 * @param CalendarEvent newCalendarEvent
+	 * @return CalenderEvent
+	 */
 	public CalendarEvent addCalendarEvent(CalendarEvent newCalendarEvent) {
 		return calendarEventRepo.save(newCalendarEvent);
 	}
-	//subtopic
+
+	/**
+	 * This method takes a CalendarSubtopic object and persists it to a database.
+	 * 
+	 * @param CalendarSubtopic newCalendarSubtopic
+	 * @return CalenderSubtopic
+	 */
 	public CalendarSubtopic addCalendarSubtopic(CalendarSubtopic newCalendarSubtopic) {
 		return calendarSubtopicRepo.save(newCalendarSubtopic);
 	}
-	
-	//update a calendar curriculum, event, or subtopic
-	//curriculum
+
+	/**
+	 * This method takes a CalendarCurriculum object and updates it in a database.
+	 * 
+	 * @param CalendarCurriculum updatedCalendarCurriculum
+	 * @return CalendarCurriculum
+	 */
 	public CalendarCurriculum updateCalendarCurriculum(CalendarCurriculum updatedCalendarCurriculum) {
-		CalendarCurriculum calendarCurriculum = calendarCurriculumRepo.findCalendarCurriculumById(updatedCalendarCurriculum.getId());	
-		if(calendarCurriculum == null) {
+		CalendarCurriculum calendarCurriculum = calendarCurriculumRepo
+				.findCalendarCurriculumById(updatedCalendarCurriculum.getId());
+		if (calendarCurriculum == null) {
 			return calendarCurriculum;
 		} else {
 			return calendarCurriculumRepo.save(updatedCalendarCurriculum);
 		}
 	}
-	//event
+
+	/**
+	 * This method takes a CalendarEvent object and updates it in a database.
+	 * 
+	 * @param CalendarEvent updatedCalendarEvent
+	 * @return CalendarEvent
+	 */
 	public CalendarEvent updateCalendarEvent(CalendarEvent updatedCalendarEvent) {
-		CalendarEvent calendarEvent = calendarEventRepo.findCalendarEventById(updatedCalendarEvent.getId());	
-		if(calendarEvent == null) {
+		CalendarEvent calendarEvent = calendarEventRepo.findCalendarEventById(updatedCalendarEvent.getId());
+		if (calendarEvent == null) {
 			return calendarEvent;
 		} else {
 			return calendarEventRepo.save(updatedCalendarEvent);
 		}
 	}
-	//subtopic
+
+	/**
+	 * This method takes a CalendarSubtopic object and updates it in a database.
+	 * 
+	 * @param CalendarSubtopic updatedCalendarSubtopic
+	 * @return CalendarSubtopic
+	 */
 	public CalendarSubtopic updateCalendarSubtopic(CalendarSubtopic updatedCalendarSubtopic) {
-		CalendarSubtopic calendarSubtopic = calendarSubtopicRepo.findCalendarSubtopicById(updatedCalendarSubtopic.getId());	
-		if(calendarSubtopic == null) {
+		CalendarSubtopic calendarSubtopic = calendarSubtopicRepo
+				.findCalendarSubtopicById(updatedCalendarSubtopic.getId());
+		if (calendarSubtopic == null) {
 			return calendarSubtopic;
 		} else {
 			return calendarSubtopicRepo.save(updatedCalendarSubtopic);
