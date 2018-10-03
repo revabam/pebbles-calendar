91f8343dc2213f71caa42460e84b0c836fca9c0c
Joshua Maciejewski <maciejewskijoshua@gmail.com>
2018-10-02 23:28:40 -0400

    fixed formatting

--- src/test/java/com/revature/tests/calendarTest/CalendarEventTest.java
+++ src/test/java/com/revature/tests/calendarTest/CalendarEventTest.java
@@ -18,6 +18,10 @@ import com.revature.tests.TestDriver;
 
 import io.restassured.RestAssured;
 
+/**
+ * Tests endpoints in the CalendarController
+ * @author Joshua Maciejewski | Spark-1806-Jun-2018-USF | Steven Kelsey
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestDriver.class, properties = "/pebbles-calendar/src/test/resources/application.properties", webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = Application.class)
@@ -25,8 +29,6 @@ public class CalendarEventTest extends TestDriver {
 
 	static String url = "http://localhost:9994/calendars";
 
-	
-	
 	@Test
 	public void findAllCalendarEvents() {
 		RestAssured.get(url + "/event").prettyPrint();
@@ -34,25 +36,25 @@ public class CalendarEventTest extends TestDriver {
 
 	@Test
 	public void findCalendarEventById() {
+		
 		int calendarEventId = RestAssured.get(url + "/event/1").as(CalendarEvent.class).getId();
+		
 		assertEquals(1, calendarEventId);
 	}
 	
-
 	@SuppressWarnings("unchecked")
 	@Test
 	public void findCalendarEventByTrainerId() {
+		
 		List<CalendarEvent> calendarEvents = RestAssured.get(url + "/event/trainer/1").as(List.class);
 		
 		assertEquals(2, calendarEvents.size());
 	}
-	
-
-	
-	
+		
 	@Test
 	@Ignore
 	public void addCalendarEvent() {
+		
 		CalendarEvent newCalendarEvent = new CalendarEvent( "Polymorphism event",
 				"This is Polymorphism event from the Polymorphism subtopic in the Core Java topic", 3,
 				new Date(1637224472690l), new Date(1937224472690l), 3, 3, 3, 3);
@@ -62,9 +64,9 @@ public class CalendarEventTest extends TestDriver {
 		assertEquals(201, status);	
 		}
 	
-	
 	@Test
 	public void updateCalendarEvent() {
+		
 		CalendarEvent updateCalendarEvent = new CalendarEvent(2, "Polymorphism event",
 				"This is Polymorphism event from the Polymorphism subtopic in the Core Java topic", 1,
 				new Date(1637224472690l), new Date(1937224472690l), 1, 1, 1, 1);
@@ -73,6 +75,5 @@ public class CalendarEventTest extends TestDriver {
 		
 		assertEquals(200, status);	
 		}
-	
-	
+
 }
